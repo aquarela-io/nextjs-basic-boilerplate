e04270947df39ade7d2a547bad710f41b697d5fb
Diego Peixoto <contato@diegopeixoto.com>
2023-08-25 12:17:35 -0300

    Initial Config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'v3-icabank-kyc.s3.amazonaws.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
